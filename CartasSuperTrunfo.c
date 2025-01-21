c3cf376a43007e30342711b3028f6b093068a039
Daniela-Vargas7 <plannear7@gmail.com>
2025-01-21 01:53:08 +0000

    iniciei o exercicio da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -17,6 +10,47 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    //ESTADO A: MATO GROSSO DO SUL
+    //primeiro passo: definir as variáveis e os tipos de dados
+
+    char cidade[50];
+    char codigo[3];
+    int populacao;
+    float area;
+    double pib;
+    int pontos;
+
+    //segundo passo: definir a entrada e saída de dados
+
+    printf("Mato Grosso do Sul \n");
+    printf("Digite o código da carta:\n");
+    scanf("%s, &codigo");
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s, &cidade");
+
+    printf("Digite o total de habitantes da cidade:\n");
+    scanf("%d, &populacao");
+
+    printf("Digite a área da cidade em metros quadrados:\n");
+    scanf("%f, &area");
+
+    printf("Digite o valor do PIB da cidade:\n");
+    scanf("%f, &pib");
+
+    printf("Quantos pontos turísticos existem na cidade?\n");
+    scanf("%d, &pontos");
+
+    //terceiro passo: imprimir as respostas do usuário
+
+    printf("Código da Carta: %s", codigo);
+    printf("Cidade: %s", cidade);
+    printf("Total de Habitantes: %d \n", populacao);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", pib);
+    printf("Quantidade de Pontos Turísticos: %d \n", pontos);
+
 
     return 0;
 }
